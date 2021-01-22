74e0a1bb190a01ef2e001cde5782bd933d9d292a
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-01-23 02:04:55 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,26 +4,23 @@
       return data.json()
     })
     .then(data => {
-      const list = document.createElement('ul')
-      list.classList.add('siteList')
+      const ul = document.createElement('ul')
+      ul.classList.add('siteList')
 
       var addSite = ({ zhName, enName, url, logo }) => {
-        const link = document.createElement('a')
-        link.href = url || '#'
-        link.target = '_blank'
-        link.innerHTML = `
-      <li>
-        <div class="title">${zhName}</div>
-        <div class="sub-title">${enName}</div>
-      </li>
-      `
-        list.insertAdjacentElement('afterbegin', link)
+        let li = document.createElement('li')
+        li.innerHTML = `
+<a href="${url}" target="_blank">
+  <div class="title">${zhName}</div>
+  <div class="sub-title">${enName}</div>
+</a>`
+        ul.insertAdjacentElement('afterbegin', li)
       }
 
       data.forEach(addSite)
 
       document
         .getElementById('siteListContainer')
-        .insertAdjacentElement('afterbegin', list)
+        .insertAdjacentElement('beforeend', ul)
     })
 })()
